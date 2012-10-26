f5ac9480f03dfdbee6a2bbcff4f370c4724d59d0
Andreas kaiser <disko@binary-punks.com>
2012-10-26 21:21:26 +0200

    Remove boilerplate code

--- kotti_theme_superhero/__init__.py
+++ kotti_theme_superhero/__init__.py
@@ -1,7 +1,3 @@
-from pyramid.i18n import TranslationStringFactory
-_ = TranslationStringFactory('kotti_theme_superhero')
-
-
 def kotti_configure(settings):
 
     settings['kotti.fanstatic.view_needed'] = 'kotti_theme_superhero.static.view_needed'
